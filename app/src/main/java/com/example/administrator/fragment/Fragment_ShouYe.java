6f0a28082a982ee2b64e9f06374a63b207337bb7
Administrator <457929376@qq.com>
2016-09-18 09:46:40 +0800

    头条appcommit模版A

---
+++ app/src/main/java/com/example/administrator/fragment/Fragment_ShouYe.java
@@ -0,0 +1,7 @@
+package com.example.administrator.fragment;
+
+/**
+ * Created by Administrator on 2016/9/18.
+ */
+public class Fragment_ShouYe {
+}
--- app/src/main/java/com/example/administrator/toutiao/demo.java
+++
@@ -1,8 +0,0 @@
-package com.example.administrator.toutiao;
-
-/**
- * Created by Administrator on 2016/9/14.
- */
-public class demo {
-    //222222
-}
